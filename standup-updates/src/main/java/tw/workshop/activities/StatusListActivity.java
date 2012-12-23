28f01686c59d2acda71411866664d1c4a3185243
sharanya <sharans@thoughtworks.com>
2012-12-23 13:42:10 +0530

    Sharan - simple list view of stories

--- standup-updates/src/main/java/tw/workshop/activities/StatusListActivity.java
+++ standup-updates/src/main/java/tw/workshop/activities/StatusListActivity.java
@@ -1,21 +1,28 @@
 package tw.workshop.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import tw.workshop.R;
 import tw.workshop.adapter.StatusAdapter;
 
-public class StatusListActivity extends Activity {
+public class StatusListActivity extends RoboActivity {
 
     private static String TAG = "standup-updates-application";
 
     StatusAdapter statusAdapter;
 
+    @InjectView(R.id.status_list)
+    ListView statusList;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status_details);
+        String[] storyList = {"story1", "story2"};
+        statusList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, storyList));
     }
 
 }
--- standup-updates/src/main/java/tw/workshop/adapter/StatusAdapter.java
+++ standup-updates/src/main/java/tw/workshop/adapter/StatusAdapter.java
@@ -1,4 +1,38 @@
 package tw.workshop.adapter;
 
-public class StatusAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import tw.workshop.model.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusAdapter extends BaseAdapter {
+    private List<Status> statusList;
+
+    public StatusAdapter() {
+        statusList = new ArrayList<Status>();
+        statusList.add(new Status("1341", "InProgress"));
+    }
+
+    @Override
+    public int getCount() {
+       return statusList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return statusList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
